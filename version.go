cf18454221d20f81761390c965cc82018fa3ee45
deadprogram <ron@hybridgroup.com>
2021-07-01 08:26:49 +0200

    all: add version ID for support purposes
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ version.go
@@ -0,0 +1,5 @@
+package tinyfs
+
+// Version returns a user-readable string showing the version of the package for support purposes.
+// Update this value before release of new version of software.
+const Version = "0.1.0"
