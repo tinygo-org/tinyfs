a696cce44a989e9cbc5078252418f6a2815e11f7
deadprogram <ron@hybridgroup.com>
2025-03-12 10:26:16 +0100

    examples: updates needed for latest tinygo/drivers using SPI pointers
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- examples/console/fatfs/spi/main.go
+++ examples/console/fatfs/spi/main.go
@@ -1,4 +1,4 @@
-// +build tinygo
+//go:build tinygo
 
 package main
 
@@ -13,7 +13,7 @@ import (
 
 var (
 	blockDevice = flash.NewSPI(
-		&machine.SPI1,
+		machine.SPI1,
 		machine.SPI1_SDO_PIN,
 		machine.SPI1_SDI_PIN,
 		machine.SPI1_SCK_PIN,
--- examples/console/littlefs/spi/main.go
+++ examples/console/littlefs/spi/main.go
@@ -1,3 +1,4 @@
+//go:build tinygo
 // +build tinygo
 
 package main
@@ -13,7 +14,7 @@ import (
 
 var (
 	blockDevice = flash.NewSPI(
-		&machine.SPI1,
+		machine.SPI1,
 		machine.SPI1_SDO_PIN,
 		machine.SPI1_SDI_PIN,
 		machine.SPI1_SCK_PIN,
